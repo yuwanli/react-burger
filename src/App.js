aa3c792f2ea060fcb10e23f28949904457c361d6
wanli.yu <wanli.yu@beibei.com>
2019-07-14 20:05:52 +0800

    update code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Layout from './components/layout/layout'
+import Layout from './hoc/layout/layout'
 import BurgerBuilder from './container/burgerBuilder/burgerBuilder'
 
 function App() {
